6b0afad2cbae20e7e1380b0db1c2146acbebccc6
Coldsp33d <7982182+Coldsp33d@users.noreply.github.com>
2018-05-28 22:08:39 -0700

    Change pdfbox version

--- pdfbox/__init__.py
+++ pdfbox/__init__.py
@@ -12,7 +12,7 @@ import urllib.request
 import appdirs
 import sarge
 
-pdfbox_version = '2.0.8'
+pdfbox_version = '2.0.9'
 pdfbox_url = 'https://www.apache.org/dist/pdfbox/{version}/pdfbox-app-{version}.jar'.format(version=pdfbox_version)
 md5_url = 'https://www.apache.org/dist/pdfbox/{version}/pdfbox-app-{version}.jar.md5'.format(version=pdfbox_version)
 
